4bc161c0ce7c05d56e487066103a9ec8a714c367
darshil-kapadia <f2015113@pilani.bits-pilani.ac.in>
2017-03-18 23:04:16 +0530

    alarm worksclear!

--- app/src/main/java/com/example/dk/trial_alarm/MainActivity.java
+++ app/src/main/java/com/example/dk/trial_alarm/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.dk.trial_alarm;
 
+import java.util.Calendar;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
@@ -10,47 +11,27 @@ import android.media.MediaPlayer;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int input_hour;
+    private int input_minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Context activity_context = getApplicationContext();
+        Log.v("Checking class", (this.getClass().toString()));
+
         // creating media player instance
         int resid = getResources().getIdentifier("lp_bs", "raw", getPackageName());
         final MediaPlayer mediaPlayer = MediaPlayer.create(this, resid);
-
-
-        BroadcastReceiver br = new BroadcastReceiver()
-        {
-            @Override
-            public void onReceive(Context c, Intent i)
-            {
-                try
-                {
-                    mediaPlayer.start();
-                }
-                catch(Exception e)
-                {
-                    e.printStackTrace();
-                }
-            }
-
-        };
-
-
-        registerReceiver(br, new IntentFilter("wakeywakey") );
-
-        PendingIntent alarmIntent = PendingIntent.getBroadcast( this, 0, new Intent("wakeywakey"), 0);
-
-        AlarmManager alarms = (AlarmManager) this.getSystemService(this.ALARM_SERVICE);
-
-        alarms.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + 20000, alarmIntent);
 
         Button stop_button = (Button) findViewById(R.id.stop_button);
         stop_button.setOnClickListener(new View.OnClickListener() {
@@ -59,6 +40,63 @@ public class MainActivity extends AppCompatActivity {
                 mediaPlayer.stop();
             }
         });
+
+        Button set_button = (Button) findViewById(R.id.set_button);
+        set_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText editTextHour = (EditText) findViewById(R.id.input_hour);
+                input_hour = Integer.parseInt(editTextHour.getText().toString());
+
+                EditText editTextMinute = (EditText) findViewById(R.id.input_minute);
+                input_minute = Integer.parseInt(editTextMinute.getText().toString());
+
+                Calendar c_set = Calendar.getInstance();
+                c_set.set(Calendar.HOUR_OF_DAY, input_hour);
+                c_set.set(Calendar.MINUTE, input_minute);
+
+                Calendar c = Calendar.getInstance();
+
+                long diff = c_set.getTimeInMillis() - c.getTimeInMillis();
+
+                Log.v("input", new Long(input_hour).toString());
+                Log.v("input min ", new Long(input_minute).toString());
+
+                //Log.v("currTime", new Long(c_set.getTimeInMillis()).toString());
+                ///Log.v("setTime", )
+                Log.v("diff", new Long(diff/(1000*60)).toString());
+
+                BroadcastReceiver br = new BroadcastReceiver()
+                {
+                    @Override
+                    public void onReceive(Context c, Intent i)
+                    {
+                        try
+                        {
+                            mediaPlayer.start();
+                        }
+                        catch(Exception e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+
+                };
+
+                registerReceiver(br, new IntentFilter("alarm_intent_filter") );
+                PendingIntent alarmIntent = PendingIntent.getBroadcast( activity_context, 0, new Intent("alarm_intent_filter"), 0);
+
+                AlarmManager alarms = (AlarmManager) activity_context.getSystemService(activity_context.ALARM_SERVICE);
+
+                alarms.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + diff, alarmIntent);
+            }
+
+
+
+        });
+
+
 
 
 
